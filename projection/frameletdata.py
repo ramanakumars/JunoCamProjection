cd1917afa0bc2c0d5519dde05e6a66a4ae1122e8
Ramanakumar Sankar <ramanakumar.shankar@gmail.com>
2023-10-04 00:32:50 -0500

    adding framelet class and moving projection to Framelet

--- projection/frameletdata.py
+++ projection/frameletdata.py
@@ -1,28 +1,94 @@
 import numpy as np
-from .globals import FRAME_WIDTH, FRAME_HEIGHT
+from .globals import FRAME_WIDTH, FRAME_HEIGHT, initializer
+from .cython_utils import project_midplane_c
+from .camera_funcs import CameraModel
+import spiceypy as spice
+import matplotlib.pyplot as plt
+from skimage import io
+import os
+import sys
+import tqdm
+import multiprocessing
 
 
 class FrameletData:
-    def __init__(self, nframes):
-        self.nframes = nframes
+    def __init__(self, metadata, imgfolder):
+        self.nframelets = int(metadata["LINES"] / FRAME_HEIGHT)
+        # number of RGB frames
+        self.nframes = int(self.nframelets / 3)
 
-        # store the pixel coordinates for the midpoint plane
-        self.coords = np.zeros((self.nframes, 3, FRAME_HEIGHT, FRAME_WIDTH, 2))
+        print(f'Determined {self.nframes} from metadata')
 
-        # corresponding pixel image values
-        self.image = np.zeros((self.nframes, 3, FRAME_HEIGHT, FRAME_WIDTH))
-        self.rawimg = np.zeros((self.nframes, 3, FRAME_HEIGHT, FRAME_WIDTH))
+        start_utc = metadata["START_TIME"]
+        self.start_et = spice.str2et(start_utc)
+        fname = metadata["FILE_NAME"].replace("-raw.png", "")
+        self.fullimg = plt.imread(imgfolder + "%s-raw.png" % fname)
 
-        # latitudes/longitudes
-        self.lat = np.zeros((self.nframes, 3, FRAME_HEIGHT, FRAME_WIDTH))
-        self.lon = np.zeros((self.nframes, 3, FRAME_HEIGHT, FRAME_WIDTH))
+        # ignore color channels in the image
+        if len(self.fullimg.shape) == 3:
+            self.fullimg = self.fullimg.mean(axis=-1)
 
-        # and the backplane information
-        self.emission = np.zeros((self.nframes, 3, FRAME_HEIGHT, FRAME_WIDTH))
-        self.incidence = np.zeros((self.nframes, 3, FRAME_HEIGHT, FRAME_WIDTH))
+        self.sclat = float(metadata["SUB_SPACECRAFT_LATITUDE"])
+        self.sclon = float(metadata["SUB_SPACECRAFT_LONGITUDE"])
 
-        # and the calibration
-        self.fluxcal = np.zeros((self.nframes, 3, FRAME_HEIGHT, FRAME_WIDTH))
+        # get the exposure and convert to seconds
+        self.exposure = (
+            float(
+                metadata["EXPOSURE_DURATION"].replace("<ms>", "").strip()
+            )
+            / 1.0e3
+        )
+
+        intframe_delay = metadata["INTERFRAME_DELAY"].split(" ")
+        frame_delay = float(intframe_delay[0])
+
+        # flatfield and gain from Brian Swift's GitHub
+        # (https://github.com/BrianSwift/JunoCam/tree/master/Juno3D)
+        self.flatfield = np.array(
+            io.imread(
+                os.path.dirname(__file__) + "/cal/flatFieldsSmooth12to16.tiff"
+            )
+        )
+        self.flatfield[self.flatfield == 0] = 1.0
+
+        self.framelets = []
+        for n in tqdm.tqdm(range(self.nframes), desc='Decompanding'):
+            for c in range(3):
+                startrow = 3 * FRAME_HEIGHT * n + c * FRAME_HEIGHT
+                endrow = 3 * FRAME_HEIGHT * n + (c + 1) * FRAME_HEIGHT
+                flati = self.flatfield[(c * FRAME_HEIGHT): ((c + 1) * FRAME_HEIGHT), :]
+
+                img = self.fullimg[startrow:endrow, :]
+                framei = decompand(img) / 16384
+                framei = framei / (flati * self.exposure)
+
+                self.framelets.append(Framelet(self.start_et, frame_delay, n, c, framei))
+
+    def get_backplane(self, num_procs):
+        tmid = np.mean([frame.et for frame in self.framelets])
+
+        '''
+        inpargs = [(frame, tmid) for frame in self.framelets]
+
+        with multiprocessing.Pool(
+            processes=num_procs, initializer=initializer
+        ) as pool:
+            try:
+                pool.starmap(Framelet.project_to_midplane, tqdm.tqdm(inpargs))
+                pool.close()
+            except KeyboardInterrupt:
+                pool.terminate()
+                pool.join()
+                sys.exit()
+
+            pool.join()
+        '''
+        for frame in tqdm.tqdm(self.framelets):
+            frame.project_to_midplane(tmid)
+
+    def update_jitter(self, jitter):
+        for framelet in self.framelets:
+            framelet.jitter = jitter
 
     def update_image(self, new_image):
         assert new_image.shape == self.image.shape, f"New image must be the same shape as the old one. Got {new_image.shape} instead of {self.image.shape}"
@@ -31,3 +97,128 @@ class FrameletData:
         self.image_old = self.image
         # then store the new image data
         self.image[:] = new_image[:]
+
+    @property
+    def tmid(self):
+        return np.mean([frame.et for frame in self.framelets])
+
+    @property
+    def image(self):
+        return np.stack([frame.image for frame in self.framelets], axis=0).reshape((self.nframes, 3, FRAME_HEIGHT, FRAME_WIDTH))
+
+    @property
+    def coords(self):
+        return np.stack([frame.coords for frame in self.framelets], axis=0).reshape((self.nframes, 3, FRAME_HEIGHT, FRAME_WIDTH, 2))
+
+    @property
+    def emission(self):
+        return np.stack([frame.emission for frame in self.framelets], axis=0).reshape((self.nframes, 3, FRAME_HEIGHT, FRAME_WIDTH))
+
+    @property
+    def incidence(self):
+        return np.stack([frame.incidence for frame in self.framelets], axis=0).reshape((self.nframes, 3, FRAME_HEIGHT, FRAME_WIDTH))
+
+    @property
+    def longitude(self):
+        return np.stack([frame.lon for frame in self.framelets], axis=0).reshape((self.nframes, 3, FRAME_HEIGHT, FRAME_WIDTH))
+
+    @property
+    def latitude(self):
+        return np.stack([frame.lat for frame in self.framelets], axis=0).reshape((self.nframes, 3, FRAME_HEIGHT, FRAME_WIDTH))
+
+
+class Framelet:
+    def __init__(self, start_et, frame_delay, frame_no, color, img):
+        self.start_et = start_et
+        self.frame_delay = frame_delay
+        self.frame_no = frame_no
+        self.color = color
+        self.rawimg = img
+        self.jitter = None
+        self.camera = CameraModel(color)
+
+    @property
+    def et(self):
+        jitter = 0 if self.jitter is None else self.jitter
+        return (
+            self.start_et
+            + self.camera.time_bias
+            + jitter
+            + (self.frame_delay + self.camera.iframe_delay) * self.frame_no
+        )
+
+    def project_to_midplane(self, tmid):
+        coords = np.nan * np.zeros((FRAME_HEIGHT, FRAME_WIDTH, 2))
+        lat = np.nan * np.zeros((FRAME_HEIGHT, FRAME_WIDTH))
+        lon = np.nan * np.zeros((FRAME_HEIGHT, FRAME_WIDTH))
+        incidence = np.nan * np.zeros((FRAME_HEIGHT, FRAME_WIDTH))
+        emission = np.nan * np.zeros((FRAME_HEIGHT, FRAME_WIDTH))
+        fluxcal = np.nan * np.zeros((FRAME_HEIGHT, FRAME_WIDTH))
+
+        project_midplane_c(self.et, self.color, tmid, lon, lat, incidence, emission, coords, fluxcal)
+
+        self.coords = coords
+        self.image = self.rawimg / fluxcal
+        self.lat = lat
+        self.lon = lon
+
+        # emission and incidence angles for lightning correction
+        self.emission = emission
+        self.incidence = incidence
+
+        # geometry correction
+        self.fluxcal = fluxcal
+
+
+# for decompanding -- taken from Kevin Gill's github page
+SQROOT = np.array(
+    (
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+        19, 20, 21, 22, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45,
+        47, 49, 51, 53, 55, 57, 59, 61, 63, 67, 71, 75, 79, 83, 87, 91,
+        95, 99, 103, 107, 111, 115, 119, 123, 127, 131, 135, 139, 143, 147,
+        151, 155, 159, 163, 167, 171, 175, 179, 183, 187, 191, 195, 199, 203,
+        207, 211, 215, 219, 223, 227, 231, 235, 239, 243, 247, 255, 263, 271,
+        279, 287, 295, 303, 311, 319, 327, 335, 343, 351, 359, 367, 375, 383,
+        391, 399, 407, 415, 423, 431, 439, 447, 455, 463, 471, 479, 487, 495,
+        503, 511, 519, 527, 535, 543, 551, 559, 567, 575, 583, 591, 599, 607,
+        615, 623, 631, 639, 647, 655, 663, 671, 679, 687, 695, 703, 711, 719,
+        727, 735, 743, 751, 759, 767, 775, 783, 791, 799, 807, 815, 823, 831,
+        839, 847, 855, 863, 871, 879, 887, 895, 903, 911, 919, 927, 935, 943,
+        951, 959, 967, 975, 983, 991, 999, 1007, 1023, 1039, 1055, 1071,
+        1087, 1103, 1119, 1135, 1151, 1167, 1183, 1199, 1215, 1231, 1247,
+        1263, 1279, 1295, 1311, 1327, 1343, 1359, 1375, 1391, 1407, 1439,
+        1471, 1503, 1535, 1567, 1599, 1631, 1663, 1695, 1727, 1759, 1791,
+        1823, 1855, 1887, 1919, 1951, 1983, 2015, 2047, 2079, 2111, 2143,
+        2175, 2207, 2239, 2271, 2303, 2335, 2367, 2399, 2431, 2463, 2495,
+        2527, 2559, 2591, 2623, 2655, 2687, 2719, 2751, 2783, 2815, 2847,
+        2879),
+    dtype=np.double
+)
+
+
+def decompand(image):
+    """
+    Decompands the image from the 8-bit in the public release
+    to the original 12-bit shot by JunoCam
+
+    Parameters
+    ----------
+    image : numpy.ndarray
+        8-bit input image
+
+    Outputs
+    -------
+    data : numpy.ndarray
+        Original 12-bit image
+    """
+    data = np.array(255 * image, dtype=np.uint8)
+    ny, nx = data.shape
+
+    def get_sqrt(x):
+        return SQROOT[x]
+    v_get_sqrt = np.vectorize(get_sqrt)
+
+    data2 = v_get_sqrt(data)
+
+    return data2
--- projection/projector.py
+++ projection/projector.py
@@ -1,13 +1,10 @@
 import numpy as np
 import json
-import re
 from skimage import feature
 from sklearn.metrics import pairwise_distances
 from sklearn.neighbors import NearestNeighbors
-import os
 import matplotlib.pyplot as plt
 import spiceypy as spice
-from skimage import io
 import tqdm
 import multiprocessing
 import time
@@ -18,59 +15,6 @@ from .camera_funcs import CameraModel
 from .spice_utils import get_kernels
 from .frameletdata import FrameletData
 import healpy as hp
-
-# for decompanding -- taken from Kevin Gill's github page
-SQROOT = np.array(
-    (
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
-        19, 20, 21, 22, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45,
-        47, 49, 51, 53, 55, 57, 59, 61, 63, 67, 71, 75, 79, 83, 87, 91,
-        95, 99, 103, 107, 111, 115, 119, 123, 127, 131, 135, 139, 143, 147,
-        151, 155, 159, 163, 167, 171, 175, 179, 183, 187, 191, 195, 199, 203,
-        207, 211, 215, 219, 223, 227, 231, 235, 239, 243, 247, 255, 263, 271,
-        279, 287, 295, 303, 311, 319, 327, 335, 343, 351, 359, 367, 375, 383,
-        391, 399, 407, 415, 423, 431, 439, 447, 455, 463, 471, 479, 487, 495,
-        503, 511, 519, 527, 535, 543, 551, 559, 567, 575, 583, 591, 599, 607,
-        615, 623, 631, 639, 647, 655, 663, 671, 679, 687, 695, 703, 711, 719,
-        727, 735, 743, 751, 759, 767, 775, 783, 791, 799, 807, 815, 823, 831,
-        839, 847, 855, 863, 871, 879, 887, 895, 903, 911, 919, 927, 935, 943,
-        951, 959, 967, 975, 983, 991, 999, 1007, 1023, 1039, 1055, 1071,
-        1087, 1103, 1119, 1135, 1151, 1167, 1183, 1199, 1215, 1231, 1247,
-        1263, 1279, 1295, 1311, 1327, 1343, 1359, 1375, 1391, 1407, 1439,
-        1471, 1503, 1535, 1567, 1599, 1631, 1663, 1695, 1727, 1759, 1791,
-        1823, 1855, 1887, 1919, 1951, 1983, 2015, 2047, 2079, 2111, 2143,
-        2175, 2207, 2239, 2271, 2303, 2335, 2367, 2399, 2431, 2463, 2495,
-        2527, 2559, 2591, 2623, 2655, 2687, 2719, 2751, 2783, 2815, 2847,
-        2879),
-    dtype=np.double
-)
-
-
-def decompand(image):
-    """
-    Decompands the image from the 8-bit in the public release
-    to the original 12-bit shot by JunoCam
-
-    Parameters
-    ----------
-    image : numpy.ndarray
-        8-bit input image
-
-    Outputs
-    -------
-    data : numpy.ndarray
-        Original 12-bit image
-    """
-    data = np.array(255 * image, dtype=np.uint8)
-    ny, nx = data.shape
-
-    def get_sqrt(x):
-        return SQROOT[x]
-    v_get_sqrt = np.vectorize(get_sqrt)
-
-    data2 = v_get_sqrt(data)
-
-    return data2
 
 
 class Projector:
@@ -89,57 +33,19 @@ class Projector:
 
     def __init__(self, imagefolder, meta, kerneldir="."):
         with open(meta, "r") as metafile:
-            self.metadata = json.load(metafile)
+            metadata = json.load(metafile)
 
-        self.start_utc = self.metadata["START_TIME"]
-        self.fname = self.metadata["FILE_NAME"].replace("-raw.png", "")
-        intframe_delay = self.metadata["INTERFRAME_DELAY"].split(" ")
+        self.start_utc = metadata["START_TIME"]
+        self.fname = metadata["FILE_NAME"].replace("-raw.png", "")
 
         print(f"Loading data for {self.fname}")
 
-        self.fullimg = plt.imread(imagefolder + "%s-raw.png" % self.fname)
-
-        # ignore color channels in the image
-        if len(self.fullimg.shape) == 3:
-            self.fullimg = self.fullimg.mean(axis=-1)
-
-        self.sclat = float(self.metadata["SUB_SPACECRAFT_LATITUDE"])
-        self.sclon = float(self.metadata["SUB_SPACECRAFT_LONGITUDE"])
-
-        # get the exposure and convert to seconds
-        self.exposure = (
-            float(
-                self.metadata["EXPOSURE_DURATION"].replace("<ms>", "").strip()
-            )
-            / 1.0e3
-        )
-
-        self.frame_delay = float(intframe_delay[0])
-
         # number of strips
-        self.nframelets = int(self.metadata["LINES"] / FRAME_HEIGHT)
-        # number of RGB frames
-        self.nframes = int(self.nframelets / 3)
-
         self.load_kernels(kerneldir)
 
-        self.re, _, self.rp = spice.bodvar(spice.bodn2c("JUPITER"), "RADII", 3)
-        self.flattening = (self.re - self.rp) / self.re
-
-        # calculate the start time
-        self.start_et = spice.str2et(self.start_utc)
-
-        self.savefolder = "%s_proj/" % self.fname
+        self.framedata = FrameletData(metadata, imagefolder)
 
         self.find_jitter(jitter_max=120)
-
-        self.et = np.zeros((self.nframes, 3))
-
-        for n in range(self.nframes):
-            for c in range(3):
-                ci = CameraModel(c)
-                self.et[n, c] = self.start_et + ci.time_bias + self.jitter +\
-                    (self.frame_delay + ci.iframe_delay) * n
 
     def load_kernels(self, KERNEL_DATAFOLDER):
         self.kernels = []
@@ -150,30 +56,23 @@ class Projector:
             self.kernels.append(kernel)
 
     def find_jitter(self, jitter_max=25, plot=False):
-        threshold = 0.1 * self.fullimg.max()
+        threshold = 0.1 * self.framedata.fullimg.max()
 
-        for nci in range(self.nframes * 3):
+        for nci in range(self.framedata.nframes * 3):
             # find whether the planet limb is in now
             # approximating this as the first time the planet is seen
             # in the image, which is generally true..
-
-            n = nci // 3
             ci = nci % 3
 
-            start = 3 * FRAME_HEIGHT * n + ci * FRAME_HEIGHT
-            end = 3 * FRAME_HEIGHT * n + (ci + 1) * FRAME_HEIGHT
-            frame = self.fullimg[start:end, :]
+            frame = self.framedata.framelets[nci].rawimg
+            eti = self.framedata.framelets[nci].et
+
             if len(frame[frame > threshold]) > 5000:
                 # make sure that the limb is also in the image
                 # we want to find a frame where both the predicted limb
                 # with no jitter and the actual limb are visible so that
                 # the offset we determine is small
                 cami = CameraModel(ci)
-                eti = (
-                    self.start_et
-                    + cami.time_bias
-                    + (self.frame_delay + cami.iframe_delay) * n
-                )
                 limb_pts = self.get_limb(eti, cami)
 
                 # bad test image if the limb is not fully visible
@@ -218,9 +117,7 @@ class Projector:
         min_dists = np.zeros_like(jitter_list)
 
         for j, jitter in enumerate(tqdm.tqdm(jitter_list, desc="Finding jitter")):
-            eti = self.start_et + cami.time_bias + jitter + (self.frame_delay + cami.iframe_delay) * n
-
-            limbs_jcam = self.get_limb(eti, cami)
+            limbs_jcam = self.get_limb(eti + jitter, cami)
 
             if len(limbs_jcam) < 1:
                 min_dists[j] = 1.0e10
@@ -251,12 +148,8 @@ class Projector:
 
         print("Found best jitter value of %.1f ms" % (self.jitter * 1.0e3))
 
-        eti = (
-            self.start_et
-            + cami.time_bias
-            + self.jitter
-            + (self.frame_delay + cami.iframe_delay) * n
-        )
+        self.framedata.update_jitter(self.jitter)
+        eti = self.framedata.framelets[nci].et
 
         limbs_jcam = self.get_limb(eti, cami)
 
@@ -298,7 +191,7 @@ class Projector:
     def process(self, nside=512, num_procs=8, apply_LS=True, n_neighbor=5):
         print(f"Projecting {self.fname} to a HEALPix grid with n_side={nside}")
 
-        self.project_to_midplane(num_procs)
+        self.framedata.get_backplane(num_procs)
 
         if apply_LS:
             self.framedata.update_image(
@@ -311,112 +204,6 @@ class Projector:
         map = self.project_to_healpix(nside, coords_new, imgvals_new, n_neighbor=n_neighbor)
 
         return map
-
-    def project_to_midplane(self, num_procs=8):
-        """
-        Main driver for the projection to a camera frame. Projects
-        a full frame onto a camera view of the planet at the middle
-        timestamp of a given image
-
-        Parameters
-        ----------
-        num_procs : int
-            Number of CPUs to use for multiprocessing [Default: 1]
-        """
-
-        # flatfield and gain from Brian Swift's GitHub
-        # (https://github.com/BrianSwift/JunoCam/tree/master/Juno3D)
-        flatfield = np.array(
-            io.imread(
-                os.path.dirname(__file__) + "/cal/flatFieldsSmooth12to16.tiff"
-            )
-        )
-        flatfield[flatfield == 0] = 1.0
-
-        inpargs = []
-        self.image = np.zeros_like(self.fullimg)
-
-        # decompand the image, apply the flat  field and get the input arguments
-        # for the multiprocessing driver
-        for n in tqdm.tqdm(range(self.nframes), desc='Decompanding'):
-            for c in range(3):
-                startrow = 3 * FRAME_HEIGHT * n + c * FRAME_HEIGHT
-                endrow = 3 * FRAME_HEIGHT * n + (c + 1) * FRAME_HEIGHT
-
-                framei = decompand(self.fullimg[startrow:endrow, :]) / 16384
-
-                flati = flatfield[
-                    (c * FRAME_HEIGHT): ((c + 1) * FRAME_HEIGHT), :
-                ]
-                framei = framei / flati
-                self.image[startrow:endrow, :] = framei / self.exposure
-                inpargs.append([self.et[n, c], n, c, np.mean(self.et)])
-
-        with multiprocessing.Pool(
-            processes=num_procs, initializer=initializer
-        ) as pool:
-            try:
-                r = pool.map_async(self._project_to_midplane, inpargs)
-                pool.close()
-
-                tasks = pool._cache[r._job]
-                ninpt = len(inpargs)
-                # run the projection using the multiprocessing driver
-                with tqdm.tqdm(total=ninpt, desc='Projecting framelets') as pbar:
-                    while tasks._number_left > 0:
-                        pbar.n = np.max([0, ninpt - tasks._number_left * tasks._chunksize])
-                        pbar.refresh()
-
-                        time.sleep(0.1)
-            except KeyboardInterrupt:
-                pool.terminate()
-                pool.join()
-                sys.exit()
-
-            pool.join()
-
-        results = r.get()
-
-        # create the data structure to hold the image and backplane info
-        self.framedata = FrameletData(self.nframes)
-
-        # fetch the coordinates and the image values
-        for jj in range(len(inpargs)):
-            loni, lati, inci, emisi, coordsi, fluxcali = results[jj]
-            # loni, lati, inci, emisi, coordsi, fluxcali = _project_to_midplane(*inpargs[jj])
-            _, i, ci, _ = inpargs[jj]
-            startrow = 3 * FRAME_HEIGHT * i + ci * FRAME_HEIGHT
-            endrow = 3 * FRAME_HEIGHT * i + (ci + 1) * FRAME_HEIGHT
-
-            # we store both the decompanded and raw images for future use
-            self.framedata.rawimg[i, ci, :, :] = self.image[startrow:endrow, :]
-
-            self.framedata.coords[i, ci, :] = coordsi
-            self.framedata.image[i, ci, :] = self.framedata.rawimg[i, ci, :, :] / fluxcali
-            self.framedata.lat[i, ci, :] = lati
-            self.framedata.lon[i, ci, :] = loni
-
-            # emission and incidence angles for lightning correction
-            self.framedata.emission[i, ci, :] = emisi
-            self.framedata.incidence[i, ci, :] = inci
-
-            # geometry correction
-            self.framedata.fluxcal[i, ci, :] = fluxcali
-
-        return self.framedata
-
-    def _project_to_midplane(self, inpargs):
-        eti, n, c, tmid = inpargs
-        coords = np.nan * np.zeros((FRAME_HEIGHT, FRAME_WIDTH, 2))
-        lat = np.nan * np.zeros((FRAME_HEIGHT, FRAME_WIDTH))
-        lon = np.nan * np.zeros((FRAME_HEIGHT, FRAME_WIDTH))
-        incidence = np.nan * np.zeros((FRAME_HEIGHT, FRAME_WIDTH))
-        emission = np.nan * np.zeros((FRAME_HEIGHT, FRAME_WIDTH))
-        fluxcal = np.nan * np.zeros((FRAME_HEIGHT, FRAME_WIDTH))
-
-        project_midplane_c(eti, c, tmid, lon, lat, incidence, emission, coords, fluxcal)
-
-        return lon, lat, incidence, emission, coords, fluxcal
 
     def project_to_healpix(self, nside, coords, imgvals, n_neighbor=4):
         # get the image extents in pixel coordinate space
@@ -435,7 +222,7 @@ class Projector:
 
         # this assumes the midplane mode where the coordinates
         # are using the Green band camera
-        et = np.mean(self.et)
+        et = self.framedata.tmid
 
         # get the spacecraft location and transformation to and from the JUNOCAM coordinates
         get_pixel_from_coords_c(np.radians(longrid), np.radians(latgrid), longrid.size, et, extents, pix)
@@ -455,7 +242,8 @@ def apply_lommel_seeliger(imgvals, incidence, emission):
     '''
         Apply the Lommel-Seeliger correction for incidence
     '''
-    # apply Lommel-Siegler correction
+    print("Applying Lommel-Seeliger correction")
+    # apply Lommel-Seeliger correction
     mu0 = np.cos(incidence)
     mu = np.cos(emission)
     corr = 1. / (mu + mu0)
