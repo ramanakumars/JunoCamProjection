81945ec89f4d8d14b6d46809a86103222ba0f956
Ramanakumar Sankar <ramanakumar.shankar@gmail.com>
2024-08-13 20:47:38 -0700

    format fixing

--- projection/project.c
+++ projection/project.c
@@ -18,22 +18,28 @@ double aperture = 1.;
 double pixel_size = 7.4e-6;
 double focal_length = 0.001095637;
 
-struct Camera {
+struct Camera
+{
   double k1, k2, cx, cy, flength, psize, f1, time_bias, iframe_delay;
 };
 
 // subtract two 3D vectors: out = x - y
-void subtract3D(double *x, double *y, double *out) {
-  for (int i = 0; i < 3; i++) {
+void subtract3D(double *x, double *y, double *out)
+{
+  for (int i = 0; i < 3; i++)
+  {
     out[i] = x[i] - y[i];
   }
 }
 
 // 3D matrix dot product: out = A dot b
-void matmul3D(double A[3][3], double *b, double *out) {
-  for (int i = 0; i < 3; i++) {
+void matmul3D(double A[3][3], double *b, double *out)
+{
+  for (int i = 0; i < 3; i++)
+  {
     out[i] = 0.;
-    for (int j = 0; j < 3; j++) {
+    for (int j = 0; j < 3; j++)
+    {
       out[i] += A[i][j] * b[j];
     }
   }
@@ -41,7 +47,8 @@ void matmul3D(double A[3][3], double *b, double *out) {
 
 // set the camera constants and transform functions
 // from https://naif.jpl.nasa.gov/pub/naif/JUNO/kernels/ik/juno_junocam_v03.ti
-int initialize_camera(struct Camera *camera, int cam) {
+int initialize_camera(struct Camera *camera, int cam)
+{
   camera->k1 = -5.9624209455667325e-08;
   camera->k2 = 2.7381910042256151e-14;
   camera->cx = 814.21;
@@ -57,7 +64,8 @@ int initialize_camera(struct Camera *camera, int cam) {
   return 1;
 }
 
-void distort(struct Camera *camera, double *c, double *campos) {
+void distort(struct Camera *camera, double *c, double *campos)
+{
   double xd, yd, r2, dr;
   xd = c[0];
   yd = c[1];
@@ -70,7 +78,8 @@ void distort(struct Camera *camera, double *c, double *campos) {
   campos[1] = yd;
 }
 
-void vec2pix(struct Camera *camera, double *v, double *pix) {
+void vec2pix(struct Camera *camera, double *v, double *pix)
+{
   double alpha, x, y;
   double cam[2];
   double campos[2];
@@ -86,11 +95,13 @@ void vec2pix(struct Camera *camera, double *v, double *pix) {
   pix[1] = y;
 }
 
-void undistort(struct Camera *camera, double *cam, double *ucam) {
+void undistort(struct Camera *camera, double *cam, double *ucam)
+{
   register double xd, yd, r2, dr;
   xd = cam[0];
   yd = cam[1];
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < 5; i++)
+  {
     r2 = xd * xd + yd * yd;
     dr = 1. + camera->k1 * r2 + camera->k2 * r2 * r2;
     xd = cam[0] / dr;
@@ -100,7 +111,8 @@ void undistort(struct Camera *camera, double *cam, double *ucam) {
   ucam[1] = yd;
 }
 
-void pix2vec(struct Camera *camera, double *px, double *vec) {
+void pix2vec(struct Camera *camera, double *px, double *vec)
+{
   double cam[2], ucam[2];
 
   cam[0] = px[0] - camera->cx;
@@ -114,7 +126,8 @@ void pix2vec(struct Camera *camera, double *px, double *vec) {
 void furnish(char *file) { furnsh_c(file); }
 
 void process(double eti, int cam, double cam2jup[3][3], double *lon, double *lat,
-             double *inclin, double *emis, double *fluxcal) {
+             double *inclin, double *emis, double *fluxcal)
+{
   double pix[2], spoint[3], srfvec[3], pixvec[3], pos_jup[3], scloc[3];
   double disti, loni, lati, phase, inc, emission, trgepoch, lt, plate_scale,
       ang_size, footprint;
@@ -128,8 +141,10 @@ void process(double eti, int cam, double cam2jup[3][3], double *lon, double *lat
 
   spkpos_c("JUNO", eti, "IAU_JUPITER", "CN+S", "JUPITER", scloc, &lt);
 
-  for (int jj = 0; jj < FRAME_HEIGHT; jj++) {
-    for (int ii = 23; ii < FRAME_WIDTH - 17; ii++) {
+  for (int jj = 0; jj < FRAME_HEIGHT; jj++)
+  {
+    for (int ii = 23; ii < FRAME_WIDTH - 17; ii++)
+    {
       // convert the pixel coordinate to a vector
       // in the JUNOCAM frame
       pix[0] = (double)ii;
@@ -143,7 +158,8 @@ void process(double eti, int cam, double cam2jup[3][3], double *lon, double *lat
       sincpt_c("Ellipsoid", "JUPITER", eti, "IAU_JUPITER", "CN+S", "JUNO",
                "IAU_JUPITER", pos_jup, spoint, &trgepoch, srfvec, &found);
 
-      if (found) {
+      if (found)
+      {
         // if the intercept exists, calculate the
         // coordinates of the intercept
         reclat_c(spoint, &disti, &loni, &lati);
@@ -168,8 +184,9 @@ void process(double eti, int cam, double cam2jup[3][3], double *lon, double *lat
         footprint = (plate_scale * disti / cos(emission));
 
         fluxcal[jj * FRAME_WIDTH + ii] = 1. / (ang_size * footprint);
-
-      } else {
+      }
+      else
+      {
         inc = 1000.;
         emission = 1000.;
         loni = -1000.;
@@ -187,7 +204,8 @@ void process(double eti, int cam, double cam2jup[3][3], double *lon, double *lat
 
 void project_midplane(double eti, int cam, double tmid, double *lon,
                       double *lat, double *incid, double *emis, double *coords,
-                      double *fluxcal) {
+                      double *fluxcal)
+{
   double pix[2], pix_transformed[2], spoint[3], srfvec[3], pixvec[3], scloc[3],
       vec_transformed[3], vec_iau[3], pxfrm_mid[3][3], pxfrm_iau[3][3], disti,
       loni, lati, phase, inc, emission, trgepoch, lt, plate_scale, cosalpha;
@@ -203,12 +221,13 @@ void project_midplane(double eti, int cam, double tmid, double *lon,
 
   pxform_c("JUNO_JUNOCAM", "IAU_JUPITER", eti, pxfrm_iau);
 
-  for (int jj = 0; jj < FRAME_HEIGHT; jj++) {
-    for (int ii = 0; ii < FRAME_WIDTH; ii++) {
+  for (int jj = 0; jj < FRAME_HEIGHT; jj++)
+  {
+    for (int ii = 0; ii < FRAME_WIDTH; ii++)
+    {
       pix[0] = ii;
       pix[1] = jj;
       pix2vec(&camera, pix, pixvec);
-
 
       matmul3D(pxfrm_iau, pixvec, vec_iau);
 
@@ -216,7 +235,8 @@ void project_midplane(double eti, int cam, double tmid, double *lon,
       sincpt_c("Ellipsoid", "JUPITER", eti, "IAU_JUPITER", "CN+S", "JUNO",
                "IAU_JUPITER", vec_iau, spoint, &trgepoch, srfvec, &found);
 
-      if (found) {
+      if (found)
+      {
         // find the coordinate of the surface point
         reclat_c(spoint, &disti, &loni, &lati);
 
@@ -241,7 +261,7 @@ void project_midplane(double eti, int cam, double tmid, double *lon,
             pixvec[2] / sqrtf(pixvec[0] * pixvec[0] + pixvec[1] * pixvec[1] +
                               pixvec[2] * pixvec[2]);
         fluxcal[jj * FRAME_WIDTH + ii] =
-            (M_PI / 4.) * pow(disti, 2.) * 
+            (M_PI / 4.) * pow(disti, 2.) *
             pow((aperture / focal_length) * cosalpha * cosalpha, 2);
       }
     }
@@ -249,7 +269,8 @@ void project_midplane(double eti, int cam, double tmid, double *lon,
 }
 
 void get_pixel_from_coords(double *lon, double *lat, int npoints, double et,
-                           double *extents, double *pix) {
+                           double *extents, double *pix)
+{
   double scloc[3], spoint[3], dvec[3], dvec_jcam[3], pixi[2], lt,
       pxfrm[3][3], x0, x1, y0, y1, percentage;
   struct Camera cam0;
@@ -263,7 +284,8 @@ void get_pixel_from_coords(double *lon, double *lat, int npoints, double et,
   spkpos_c("JUNO", et, "IAU_JUPITER", "CN+S", "JUPITER", scloc, &lt);
   pxform_c("IAU_JUPITER", "JUNO_JUNOCAM", et, pxfrm);
 
-  for (int i = 0; i < npoints; i++) {
+  for (int i = 0; i < npoints; i++)
+  {
     double lati, loni;
     lati = lat[i];
     loni = lon[i];
@@ -273,9 +295,10 @@ void get_pixel_from_coords(double *lon, double *lat, int npoints, double et,
     matmul3D(pxfrm, dvec, dvec_jcam);
     vec2pix(&cam0, dvec_jcam, pixi);
 
-    if ((((dvec[0] * spoint[0] + dvec[1] * spoint[1] + dvec[2] * spoint[2]) <
-          0)) &
-        (pixi[0] >= x0) & (pixi[0] <= x1) & (pixi[1] >= y0) & (pixi[1] <= y1)) {
+    if (
+        (((dvec[0] * spoint[0] + dvec[1] * spoint[1] + dvec[2] * spoint[2]) < 0)) &
+        (pixi[0] >= x0) & (pixi[0] <= x1) & (pixi[1] >= y0) & (pixi[1] <= y1))
+    {
       pix[i * 2] = pixi[0];
       pix[i * 2 + 1] = pixi[1];
     }
