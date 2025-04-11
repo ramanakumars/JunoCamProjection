bd66598a52e554c92546deb2649fce33a0f94424
Ramanakumar Sankar <ramanakumar.shankar@gmail.com>
2025-04-10 19:36:12 -0700

    fixing mosaicing pipeline

--- junocam_projection/mosaic.py
+++ junocam_projection/mosaic.py
@@ -42,40 +42,47 @@ def mosaic_median(maps: np.ndarray) -> np.ndarray:
 
 def lowpass(data: np.ndarray, sigma: float):
     """
-    Simple Gaussian filter with a size of sigma pixels. Accounts for zonal and meridional boundaries
+    Simple uniform filter with a size of sigma pixels. Accounts for zonal and meridional boundaries
 
     :param data: input data to be filtered (must be 2D)
     :param sigma: filter size in pixels
 
-    :return: low-pass filtered image (Gaussian blurred image)
+    :return: low-pass filtered image (blurred image)
     """
     return uniform_filter(data, size=sigma, mode=['nearest', 'wrap'])
 
 
-def highpass(mapi: np.ndarray, sigma_cut: float, sigma_filter: float):
+def get_footprint(Ls: np.ndarray, sigma_cut: float):
     """
-    Apply a high pass filter by subtracting a Gaussian blurred image.
     Truncates the edges by applying a low-pass filter on the image footprint and trimming at 95% level
 
-    :param mapi: input map (must be RGB)
-    :param sigma_cut: filter width for edge detection and truncation
-    :param sigma_filter: filter width for high-pass filter
+    :param Ls: input brightness channel of the image (essentially grayscale version of image)
+    :param sigma_cut: filter width for edge detection (to truncate high frequency signal at the image edges)
 
-    :returns:   - high pass filtered image
-                - truncated footprint of the filtered image
+    :return: footprint of the image with the edges truncated
     """
-    Ls = mapi.mean(-1)
-
     # the footprint is the regions in the map where there is data
     # the minimum threshold is the 1-percentile value
     footprint_orig = np.asarray(Ls > np.percentile(Ls[Ls > 0], 1), dtype=np.float32)
-
-    # we do a gaussian blur on this footprint so that we can figure out the edges
-    footprint = np.repeat(lowpass(footprint_orig, sigma_cut)[:, :, np.newaxis], 3, axis=-1)
+    footprint = lowpass(footprint_orig, sigma_cut)
 
     # mask the edges
     footprint[footprint < 0.95] = 0
     footprint[footprint >= 0.95] = 1
+
+    return footprint
+
+
+def highpass(mapi: np.ndarray, sigma_filter: float):
+    """
+    Apply a high pass filter by subtracting a Gaussian blurred image.
+
+    :param mapi: input map (must be RGB)
+    :param sigma_filter: filter width for high-pass filter
+
+    :return: high pass filtered image
+    """
+    Ls = mapi.mean(-1)
 
     # we also apply a gaussian blur on the image brightness
     # this blur is the low-frequency signal
@@ -83,12 +90,35 @@ def highpass(mapi: np.ndarray, sigma_cut: float, sigma_filter: float):
 
     # we filter the map on the low-frequency to get only the high-frequency components
     # and cut off the edges
-    filtered = (mapi - low_freq) * footprint
+    filtered = mapi - low_freq
 
-    return filtered, footprint
+    return filtered
 
 
-def blend_maps(maps: np.ndarray, sigma_filter: float = 40, sigma_cut: float = 50) -> np.ndarray:
+def highpass_luminance_correction(mapi: np.ndarray, sigma_filter: float = 50):
+    """
+    Flatten the illumination geometry by applying a high-pass filter on the luminance data
+
+    :param mapi: input map (must be RGB)
+    :param sigma_cut: filter width for edge detection (to truncate high frequency signal at the image edges)
+
+    :return: high-pass filtered image
+    """
+    Ls = mapi.mean(-1)
+
+    # we also apply a gaussian blur on the image brightness
+    # this blur is the low-frequency signal
+    low_freq = np.repeat(lowpass(Ls, sigma_filter)[:, :, np.newaxis], 3, axis=-1)
+
+    # we filter the map on the low-frequency to get only the high-frequency components
+    # and cut off the edges
+    filtered = mapi / (low_freq + 1e-6)
+    filtered[~np.isfinite(filtered)] = 0.
+
+    return filtered
+
+
+def blend_maps(maps: np.ndarray, sigma_luminance: float = 50, sigma_filter: float = 40, sigma_cut: float = 50) -> np.ndarray:
     '''
     Combine the images using the USGS/ISIS no-seam technique. This works by applying a low-pass filter on a mosaic,
     and then combining the low-pass mosaic with a mosaic of the high-pass filtered data, which tends to remove seams efficiently.
@@ -100,19 +130,27 @@ def blend_maps(maps: np.ndarray, sigma_filter: float = 40, sigma_cut: float = 50
     # convert to float32 to speed up the calculation
     maps = maps.astype(np.float32)
 
-    # filter the input maps by truncating small values
-    # and adjusting the input values to match each other
-    for i, mapi in enumerate(tqdm.tqdm(maps, desc="Trimming bad pixels")):
-        mapi = mapi / np.median(mapi[mapi > 0])
-        mapi[mapi < 1e-4] = 0.
-        mapi[~np.isfinite(mapi)] = 0.
-        maps[i] = mapi
-
     # open the file and load the variables
     nfiles, nlat, nlon, _ = maps.shape
 
+    maps_filtered = np.zeros_like(maps)
+
+    # filter the input maps by truncating small values
+    # and adjusting the input values to match each other
+    footprints = np.zeros((nfiles, nlat, nlon, 3))
+    for i, mapi in enumerate(tqdm.tqdm(maps, desc="Applying luminance high pass")):
+        footprints[i] = np.repeat(get_footprint(mapi.mean(-1), sigma_cut)[:, :, np.newaxis], 3, axis=2)
+
+        mapi = highpass_luminance_correction(mapi, sigma_luminance)
+        mapi = mapi / np.median(mapi[mapi > 0])
+        mapi[mapi < 1e-4] = 0.
+        mapi[~np.isfinite(mapi)] = 0.
+
+        maps_filtered[i] = mapi
+    footprint = np.clip(footprints.sum(0), 0, 1)
+
     print("Creating initial mosaic")
-    mosaic_initial = mosaic_median(maps)
+    mosaic_initial = mosaic_median(maps_filtered)
 
     # apply a low pass on the initial mosaic
     lowpass_mosaic = np.zeros_like(mosaic_initial)
@@ -121,19 +159,15 @@ def blend_maps(maps: np.ndarray, sigma_filter: float = 40, sigma_cut: float = 50
 
     # then high-pass filter the maps
     highpass_data = np.zeros_like(maps, dtype=np.float32)
-    footprint = np.zeros((nlat, nlon))
-    for i, mapi in enumerate(tqdm.tqdm(maps, desc="Applying high-pass filter")):
-        highpass_data[i], footprinti = highpass(mapi, sigma_cut, sigma_filter)
-        footprint += footprinti.mean(-1)
+    for i, mapi in enumerate(tqdm.tqdm(maps_filtered, desc="Applying high-pass filter")):
+        highpass_data[i] = highpass(mapi, sigma_filter) * footprints[i]
 
         # trim the edges again since we tend to end up with certain peaks
         highpass_data[i][highpass_data[i] > np.percentile(highpass_data[i][np.abs(highpass_data[i]) > 0], 99)] = 0.
-
-    footprint = np.clip(footprint, 0, 1)
 
     # create a mosaic using the high-pass data
     print("Creating high-pass mosaic")
     mosaic_highpass = mosaic_median(highpass_data)
 
     # the final mosaic is the combination of the low-pass mosaic and the high-pass mosaic
-    return (mosaic_highpass + lowpass_mosaic) * np.repeat(footprint[:, :, np.newaxis], 3, axis=2)
+    return (mosaic_highpass + lowpass_mosaic) * footprint
