406cce45d6d0c0a3ae7b586fbbe3efe9be1be1e8
Ramanakumar Sankar <ramanakumar.shankar@gmail.com>
2025-04-10 20:33:39 -0700

    changing documentation theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -93,7 +93,12 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
+html_theme_options = {
+    "repository_url": "https://github.com/ramanakumars/JunoCamProjection",
+    "repository_branch": "master",
+    "use_repository_button": True
+}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
